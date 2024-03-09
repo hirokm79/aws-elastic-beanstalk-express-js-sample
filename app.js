214c1d4cbf216bd297070af1cae70ab6a1120c7d
hirokm79 <kmhirose14@gmail.com>
2024-03-10 01:38:54 +0900

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Kimiko!'));
+app.get('/', (req, res) => res.send('Kimiko!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
